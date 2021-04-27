5d98d852621b2d61c6b2dfcb27509bb0ba2bfacf
ttbarter317 <83311150+ttbarter317@users.noreply.github.com>
2021-04-27 13:21:54 -0700

    Delete mort_d21_geno_trans.R

--- mort_d21_geno_trans.R
+++
@@ -1,7 +0,0 @@
-setwd("~/Dropbox/Research Project/FLAM/Geno vs Trans")
-trans<-read.csv("mort_d21_alltime.csv",header=TRUE)
-trans<-as.matrix(trans[1:19,3])
-snp<-read.csv("mort_d21_geno_only.csv",header=TRUE)
-together<-read.csv("geno_trans_snp_causal.csv",header=TRUE)
-trans.together<-together[together[,1] %in% trans,]
-library(tidyr)
